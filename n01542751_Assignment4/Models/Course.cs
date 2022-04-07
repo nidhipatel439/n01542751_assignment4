3fd3a278574cda2900646a2d7d2d923e479c283a
Nidhi <nidhipatel439@gmail.com>
2022-04-06 20:20:57 -0400

    add and delete option

---
+++ n01542751_Assignment4/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace n01542751_Assignment4.Models
+{
+    public class Course
+    {
+        public string ClassName { get; set; }
+
+    }
+}
---
+++ n01542751_Assignment4/Models/TeacherCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace n01542751_Assignment4.Models
+{
+    public class TeacherCourse
+    {
+        public Teacher Teacher { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+}
